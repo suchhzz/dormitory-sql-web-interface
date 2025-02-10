349addabc469532c5f37c1dfa8c27ce83123e037
suchhzz <jenjer444@gmail.com>
2025-02-11 00:54:43 +0200

    base server file init

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+exports.createUser = (req, res, next) => {
+    try {
+        const { name, password } = req.body;
+
+        if (!name || !password) {
+            throw new Error('invalid values');
+        }
+
+        res.json({ message: 'user created',
+                    data: { name, password }
+         });
+    }
+    catch (e) {
+        next(error);
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,11 @@
-import express from "express"
+import express from 'express';
+import router from './routes/userRoutes';  // Убедитесь, что путь правильный
+
 const app = express();
 app.use(express.json());
 
+app.use('/api', router);
+
 app.listen(8080, () => {
-  console.log("Сервер запущен на http://localhost:8080");
-});
-
-app.get('/test', (req, res) => {
-  const test = "test";
-
-  const testObject = {
-    name: "test name from object",
-  }
-
-  const data = {
-    stringTest: "dasdas",
-    numberTest: 123123,
-    objectTest: testObject
-  };
-
-  res.json(data);
+  console.log('Сервер запущен на http://localhost:8080');
 });
